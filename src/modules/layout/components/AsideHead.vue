98cfe2301c04861c2fb406b8f8ef2d20cc4f4712
Nelkor <nelkor9855118751@gmail.com>
2021-05-04 19:27:11 +0300

    aside-head

--- src/modules/layout/components/AsideHead.vue
+++ src/modules/layout/components/AsideHead.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="header">
+  <div class="aside-header">
     <h2>Добавление операции</h2>
     <div class="sub">
       <div class="field">
-        <div class="icon" />
-        <div class="name">
-          <div class="name-title">ПОЛЕ</div>
+        <IconField />
+        <div>
+          <div class="thick-title">Поле</div>
           <div class="number">112</div>
         </div>
       </div>
@@ -13,16 +13,23 @@
       <div class="culture">
         <div class="name">
           <div class="first-name">Озимая пшеница</div>
-          <div class="last-name">ПОБЕДА 50</div>
+          <div class="thick-title">Победа 50</div>
         </div>
-        <div class="icon" />
+        <IconWheat />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import IconField from '@ui/icons/IconField.vue'
+import IconWheat from '@ui/icons/IconWheat.vue'
+
 export default {
   name: 'AsideHead',
+  components: {
+    IconField,
+    IconWheat,
+  },
 }
 </script>
---
+++ src/modules/ui/icons/IconField.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    width="40"
+    height="40"
+    viewBox="0 0 40 40"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M7.3762 7.10742L32.2373 6.52734L32.9102 33.5312L7.24609 33.2422L7.3762 7.10742Z"
+      fill="#73FF82"
+      stroke="#66CC66"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IconField',
+}
+</script>
